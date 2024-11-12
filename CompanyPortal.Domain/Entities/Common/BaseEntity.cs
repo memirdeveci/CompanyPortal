9d339da2cfa585d4dfa76d378965ef022f72fd7b
memirdeveci <120682306+memirdeveci@users.noreply.github.com>
2024-11-12 22:04:21 +0300

    related nuget packages added

---
+++ CompanyPortal.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CompanyPortal.Domain.Entities.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public virtual bool Status { get; set; } = true;
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
